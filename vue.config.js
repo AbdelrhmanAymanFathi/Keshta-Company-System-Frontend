28726f40d599046558ccab08ee5233a7614a8bd4
AbdelrhmanAymanFathi <abdelrhman.ayman.fathi@gmail.com>
2026-01-17 17:12:14 +0100

    update ws (web socket)

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,41 @@
+// const webpack = require('webpack');
+
+// module.exports = {
+  
+//   lintOnSave: false,       //  عامله مشكله
+//   configureWebpack: {
+//     plugins: [
+//       new webpack.ProgressPlugin()
+//     ]
+//   }
+// }
+
+// new code:
 const webpack = require('webpack');
 
 module.exports = {
-  
-  lintOnSave: false,       //  عامله مشكله
+  lintOnSave: false,
+
+  devServer: {
+    host: '0.0.0.0',   // يخلي السيرفر يسمع من بره
+    port: 8080,
+
+    client: {
+      webSocketURL: 'ws://38.242.152.149:8080/ws'
+    },
+
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8080',
+        changeOrigin: true,
+        ws: true
+      }
+    }
+  },
+
   configureWebpack: {
     plugins: [
       new webpack.ProgressPlugin()
     ]
   }
-}
+};
